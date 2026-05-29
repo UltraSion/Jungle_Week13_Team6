ba687986b1b59218f9002894314dddefd05abd92
Park SangHyeok <tkdgurehf00@gmail.com>
2026-05-30 01:39:15 +0900

    Temp

---
+++ KraftonEngine/Source/Engine/PhysicsEngine/ConstraintTypes.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Core/Types/CoreTypes.h"
+
+enum class EConstraintMotion : uint8
+{
+	Free,
+	Limited,
+	Locked
+};
--- KraftonEngine/Source/Engine/PhysicsEngine/PhysicsConstraintTemplate.h
+++ KraftonEngine/Source/Engine/PhysicsEngine/PhysicsConstraintTemplate.h
@@ -1,10 +1,37 @@
 #pragma once
 
 #include "Object/Object.h"
+#include "ConstraintInstance.h"
+
+struct FPhysicsConstraintProfileHandle
+{
+	FConstraintProfileProperties ProfileProperties;
+	FName ProfileName;
+};
 
 class UPhysicsConstraintTemplate : public UObject
 {
 public:
 	UPhysicsConstraintTemplate() = default;
 	~UPhysicsConstraintTemplate() override = default;
+
+	FConstraintInstance DefaultInstance;
+
+	const TArray<FPhysicsConstraintProfileHandle>& GetProfileHandles() const
+	{
+		return ProfileHandles;
+	}
+
+	TArray<FPhysicsConstraintProfileHandle>& GetProfileHandlesMutable()
+	{
+		return ProfileHandles;
+	}
+
+	bool ContainsConstraintProfile(FName ProfileName) const;
+	void AddConstraintProfile(FName ProfileName);
+	void RemoveConstraintProfile(FName ProfileName);
+	void ApplyConstraintProfile(FName ProfileName, FConstraintInstance& ConstraintInstance, bool bDefaultIfNotFound) const;
+
+private:
+	TArray<FPhysicsConstraintProfileHandle> ProfileHandles;
 };
